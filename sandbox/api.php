3126ad292c0289b4559e37beea04ceda6d840e05
kcliving <42876416+kcliving@users.noreply.github.com>
2019-10-02 10:07:55 -0400

    Update api.php

--- sandbox/api.php
+++ sandbox/api.php
@@ -1,5 +1,5 @@
 <?php
-
+    print_r($_SERVER);
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
